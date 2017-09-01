7aa840d73a917afe735e31c02df29e92dc70b9a4
Cactuar <7.11.open@gmail.com>
2017-09-01 13:06:47 +0800

    加入專案檔案。

---
+++ testConnect/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace testConnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mysqlButton = new System.Windows.Forms.Button();
+            this.mysqlGridView1 = new System.Windows.Forms.DataGridView();
+            this.MDBButton = new System.Windows.Forms.Button();
+            this.MDBGridView2 = new System.Windows.Forms.DataGridView();
+            this.compareGrid = new System.Windows.Forms.Button();
+            this.compareResult = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mysqlGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MDBGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mysqlButton
+            // 
+            this.mysqlButton.Location = new System.Drawing.Point(22, 51);
+            this.mysqlButton.Name = "mysqlButton";
+            this.mysqlButton.Size = new System.Drawing.Size(75, 23);
+            this.mysqlButton.TabIndex = 0;
+            this.mysqlButton.Text = "mysql";
+            this.mysqlButton.UseVisualStyleBackColor = true;
+            this.mysqlButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mysqlGridView1
+            // 
+            this.mysqlGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mysqlGridView1.Location = new System.Drawing.Point(22, 80);
+            this.mysqlGridView1.Name = "mysqlGridView1";
+            this.mysqlGridView1.RowTemplate.Height = 24;
+            this.mysqlGridView1.Size = new System.Drawing.Size(450, 337);
+            this.mysqlGridView1.TabIndex = 1;
+            // 
+            // MDBButton
+            // 
+            this.MDBButton.Location = new System.Drawing.Point(486, 51);
+            this.MDBButton.Name = "MDBButton";
+            this.MDBButton.Size = new System.Drawing.Size(75, 23);
+            this.MDBButton.TabIndex = 2;
+            this.MDBButton.Text = "MDB";
+            this.MDBButton.UseVisualStyleBackColor = true;
+            this.MDBButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MDBGridView2
+            // 
+            this.MDBGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MDBGridView2.Location = new System.Drawing.Point(486, 80);
+            this.MDBGridView2.Name = "MDBGridView2";
+            this.MDBGridView2.RowTemplate.Height = 24;
+            this.MDBGridView2.Size = new System.Drawing.Size(477, 337);
+            this.MDBGridView2.TabIndex = 3;
+            this.MDBGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.MDBGridView2_CellContentClick);
+            // 
+            // compareGrid
+            // 
+            this.compareGrid.Location = new System.Drawing.Point(22, 424);
+            this.compareGrid.Name = "compareGrid";
+            this.compareGrid.Size = new System.Drawing.Size(75, 23);
+            this.compareGrid.TabIndex = 4;
+            this.compareGrid.Text = "compareGrid";
+            this.compareGrid.UseVisualStyleBackColor = true;
+            this.compareGrid.Click += new System.EventHandler(this.compareGrid_Click);
+            // 
+            // compareResult
+            // 
+            this.compareResult.Location = new System.Drawing.Point(22, 453);
+            this.compareResult.Name = "compareResult";
+            this.compareResult.Size = new System.Drawing.Size(941, 151);
+            this.compareResult.TabIndex = 5;
+            this.compareResult.Text = "";
+            this.compareResult.TextChanged += new System.EventHandler(this.compareResult_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 616);
+            this.Controls.Add(this.compareResult);
+            this.Controls.Add(this.compareGrid);
+            this.Controls.Add(this.MDBGridView2);
+            this.Controls.Add(this.MDBButton);
+            this.Controls.Add(this.mysqlGridView1);
+            this.Controls.Add(this.mysqlButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mysqlGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MDBGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button mysqlButton;
+        private System.Windows.Forms.DataGridView mysqlGridView1;
+        private System.Windows.Forms.Button MDBButton;
+        private System.Windows.Forms.DataGridView MDBGridView2;
+        private System.Windows.Forms.Button compareGrid;
+        private System.Windows.Forms.RichTextBox compareResult;
+    }
+}
+
---
+++ testConnect/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data.OleDb;
+
+namespace testConnect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dbHost = "127.0.0.1";//資料庫位址
+            string dbUser = "root";//資料庫使用者帳號
+            string dbPass = "ganto1184";//資料庫使用者密碼
+            string dbName = "stocker";//資料庫名稱
+         
+            string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" +
+                dbPass + ";database=" + dbName;
+            MySqlConnection conn = new MySqlConnection(connStr);
+            MySqlCommand command = conn.CreateCommand();
+
+            conn.Open();
+
+            //建立 DataSet
+            DataSet dataSet = new DataSet();
+
+            //使用 MySqlDataAdapter 查詢資料，並將結果存回 DataSet 當做名為 test1 的 DataTable
+            string sql = "SELECT * " +
+                "FROM collect1\n" +
+                "WHERE tdate like '170901%'\n" +
+                "ORDER BY serial";
+            MySqlDataAdapter dataAdapter1 = new MySqlDataAdapter(sql, conn);
+            dataAdapter1.Fill(dataSet, "test1");
+
+            // test1 的 DataTable
+            DataTable dataTable = dataSet.Tables["test1"];
+
+            mysqlGridView1.DataSource = dataSet;
+            mysqlGridView1.DataMember = "test1";
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT serial, Count(serial) AS serial之筆數 " +
+                            "FROM collect1\n"+
+                            "WHERE tdate like '170901%'\n" +
+                            "GROUP BY serial\n"+
+                            "ORDER BY serial";
+
+            DataTable dt = GetOleDbDataTable("pdadata2.mdb", sql);
+            MDBGridView2.DataSource = dt;
+        }
+
+
+        private void compareGrid_Click(object sender, EventArgs e)
+        {
+            string[] mysqlserial = new string[mysqlGridView1.RowCount];
+            string[] MDBserial = new string[MDBGridView2.RowCount];
+            int i = 0;
+            foreach (DataGridViewRow dr in MDBGridView2.Rows)
+            {
+                string col = Convert.ToString(dr.Cells["serial"].Value);
+                MDBserial[i] = col;
+                i++;
+            }
+            
+            int y = 0;
+            foreach (DataGridViewRow dr in mysqlGridView1.Rows)
+            {
+                string col = Convert.ToString(dr.Cells["serial"].Value);
+                mysqlserial[y] = col;
+                y++;
+            }
+            compareResult.Text = CompareSerial(mysqlserial, MDBserial);
+            if (CompareSerial(mysqlserial, MDBserial) == "")
+            {
+                MessageBox.Show("正常");
+            }
+        }
+
+        public static string CompareSerial(string[] sql, string[] mdb)
+        {
+            var intersect = sql.Intersect(mdb);
+            var diff = mdb.Except(intersect);
+            string test = "";
+            foreach (string s in diff)
+            {
+                test = test + s + "\n";
+            }
+            return test;
+        }
+
+        public static OleDbConnection OleDbOpenConn(string Database)
+        {
+            string cnstr = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Database);
+            OleDbConnection icn = new OleDbConnection();
+            icn.ConnectionString = cnstr;
+            if (icn.State == ConnectionState.Open) icn.Close();
+            icn.Open();
+            return icn;
+        }
+
+        public static DataTable GetOleDbDataTable(string Database, string OleDbString)
+        {
+            DataTable myDataTable = new DataTable();
+            OleDbConnection icn = OleDbOpenConn(Database);
+            OleDbDataAdapter da = new OleDbDataAdapter(OleDbString, icn);
+            DataSet ds = new DataSet();
+            ds.Clear();
+            da.Fill(ds);
+            myDataTable = ds.Tables[0];
+            if (icn.State == ConnectionState.Open) icn.Close();
+            return myDataTable;
+        }
+
+        private void MDBGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+
+        private void compareResult_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
